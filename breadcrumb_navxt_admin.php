0e6dfcafae13aff13b621028641308e35faf51b3
John Havlik <mtekkmonkey@gmail.com>
2012-05-12 19:19:02 -0500

    Had one instance where ordered printf characters need to be used

--- breadcrumb_navxt_admin.php
+++ breadcrumb_navxt_admin.php
@@ -33,7 +33,7 @@ if(version_compare(phpversion(), '5.2.0', '<'))
 	//Only purpose of this function is to echo out the PHP version error
 	function bcn_phpold()
 	{
-		printf('<div class="error"><p>' . __('Your PHP version is too old, please upgrade to a newer version. Your version is %s, Breadcrumb NavXT requires %s', 'breadcrumb-navxt') . '</p></div>', phpversion(), '5.2.0');
+		printf('<div class="error"><p>' . __('Your PHP version is too old, please upgrade to a newer version. Your version is %1$s, Breadcrumb NavXT requires %2$s', 'breadcrumb-navxt') . '</p></div>', phpversion(), '5.2.0');
 	}
 	//If we are in the admin, let's print a warning then return
 	if(is_admin())

6ae8f2e8dbdda043f01a55399a0530dd5c1d1e8b
John Havlik <mtekkmonkey@gmail.com>
2012-08-01 22:24:39 -0500

    Let's block access on this file too

--- breadcrumb_navxt_class.php
+++ breadcrumb_navxt_class.php
@@ -16,7 +16,7 @@
     along with this program; if not, write to the Free Software
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
-
+require_once(dirname(__FILE__) . '/includes/block_direct_access.php');
 //The breadcrumb class
 class bcn_breadcrumb
 {

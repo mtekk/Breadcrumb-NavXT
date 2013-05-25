a130ddbe31bb3f73faa43b15b8a0da748e6dbf0a
John Havlik <mtekkmonkey@gmail.com>
2013-05-24 22:26:38 -0500

    Giving bcn_breadcrumb_url the types array and the current resource ID

--- breadcrumb_navxt_class.php
+++ breadcrumb_navxt_class.php
@@ -106,7 +106,7 @@ class bcn_breadcrumb
 	 */
 	public function set_url($url)
 	{
-		$this->url = esc_url(apply_filters('bcn_breadcrumb_url', $url));
+		$this->url = esc_url(apply_filters('bcn_breadcrumb_url', $url, $this->type, $this->id));
 		//Set linked to true if we set a non-null $url
 		if($url)
 		{

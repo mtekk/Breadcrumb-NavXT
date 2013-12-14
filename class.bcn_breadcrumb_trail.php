87ec7b6dcec5e5f6a853957ad7f15a6622a2dcd2
John Havlik <mtekkmonkey@gmail.com>
2013-12-13 22:08:43 -0600

    Fix issue where display_list() output two HTML classes per li element. Fixes #58

--- class.bcn_breadcrumb_trail.php
+++ class.bcn_breadcrumb_trail.php
@@ -969,21 +969,17 @@ class bcn_breadcrumb_trail
 			//On the first run we need to add in a class for the home breadcrumb
 			if($trail_str === '<li')
 			{
-				$trail_str .= ' class="home';
 				$li_class .= ' class="home';
 				if($key === 0)
 				{
-					$trail_str .= ' current_item';
 					$li_class .= ' current_item';
 				}
-				$trail_str .= '"';
 				$li_class .= '"';
 			}
 			//If we are on the current item there are some things that must be done
 			else if($key === 0)
 			{
 				//Add in a class for current_item
-				$trail_str .= ' class="current_item"';
 				$li_class .= ' class="current_item"';
 			}
 			//Filter li_attributes adding attributes to the li element

ae86a2b3e36a8311cd3d0d6d117ce786399800d7
John Havlik <john.havlik@mtekk.us>
2014-06-09 21:39:26 -0500

    To 5.1 we go

--- class.bcn_breadcrumb_trail.php
+++ class.bcn_breadcrumb_trail.php
@@ -21,7 +21,7 @@ require_once(dirname(__FILE__) . '/includes/block_direct_access.php');
 class bcn_breadcrumb_trail
 {
 	//Our member variables
-	private $version = '5.0.90';
+	private $version = '5.1.0';
 	//An array of breadcrumbs
 	public $breadcrumbs = array();
 	public $trail = array();

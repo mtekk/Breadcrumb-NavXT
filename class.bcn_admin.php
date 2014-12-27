6a9eee056ec27c62dbf154e66a3a1c3090b90258
John Havlik <john.havlik@mtekk.us>
2014-12-27 16:36:17 -0600

    Version bump for new features, we're closer to a beta for 5.2

--- class.bcn_admin.php
+++ class.bcn_admin.php
@@ -42,7 +42,7 @@ if(!class_exists('mtekk_adminKit'))
  */
 class bcn_admin extends mtekk_adminKit
 {
-	const version = '5.1.50';
+	const version = '5.1.60';
 	protected $full_name = 'Breadcrumb NavXT Settings';
 	protected $short_name = 'Breadcrumb NavXT';
 	protected $access_level = 'manage_options';

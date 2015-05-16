23a456ac9f64ae5d1adff3faabd2a7d105b1f562
John Havlik <john.havlik@mtekk.us>
2015-05-15 20:08:38 -0500

    Revert "Check the parent ID of an attachment to see if it is the front page before continuing. Fixes #109"
    This reverts commit 14c634f174e95db7e2588dceacbd4d5d68e7a572.

--- class.bcn_breadcrumb_trail.php
+++ class.bcn_breadcrumb_trail.php
@@ -422,16 +422,10 @@ class bcn_breadcrumb_trail
 			//Add the link
 			$breadcrumb->set_url(get_permalink());
 		}
-		//Done with the current item, now on to the parents
-		$frontpage = get_option('page_on_front');
-		//If there is a parent page, and it's not the frontpage, let's find it
-		if($post->post_parent && $post->ID != $post->post_parent && $frontpage != $post->post_parent)
-		{
-			//Get the parent's information
-			$parent = get_post($post->post_parent);
-			//Take care of the parent's breadcrumb
-			$this->do_post($parent);
-		}
+		//Get the parent's information
+		$parent = get_post($post->post_parent);
+		//Take care of the parent's breadcrumb
+		$this->do_post($parent);
 	}
 	/**
 	 * A Breadcrumb Trail Filling Function

b8290c7a7055abc79709c072668d6bb31b6272ff
John Havlik <john.havlik@mtekk.us>
2017-11-10 22:06:35 -0600

    Prep for 6.0, fix travis.ci matrix

--- class.bcn_admin.php
+++ class.bcn_admin.php
@@ -42,7 +42,7 @@ if(!class_exists('mtekk_adminKit'))
  */
 class bcn_admin extends mtekk_adminKit
 {
-	const version = '5.9.70';
+	const version = '5.9.90';
 	protected $full_name = 'Breadcrumb NavXT Settings';
 	protected $short_name = 'Breadcrumb NavXT';
 	protected $access_level = 'manage_options';
--- class.bcn_network_admin.php
+++ class.bcn_network_admin.php
@@ -27,7 +27,7 @@ if(!class_exists('bcn_admin'))
  */
 class bcn_network_admin extends bcn_admin
 {
-	const version = '5.9.70';
+	const version = '5.9.90';
 	protected $full_name = 'Breadcrumb NavXT Network Settings';
 	protected $access_level = 'manage_network_options';
 	/**

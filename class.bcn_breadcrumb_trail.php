657df267df0edffeea75a5872f17c99921effe0b
John Havlik <john.havlik@mtekk.us>
2024-12-01 22:13:47 -0600

    7.4 development version bump

--- class.bcn_breadcrumb_trail.php
+++ class.bcn_breadcrumb_trail.php
@@ -21,7 +21,7 @@ require_once(dirname(__FILE__) . '/includes/block_direct_access.php');
 class bcn_breadcrumb_trail
 {
 	//Our member variables
-	const version = '7.3.1';
+	const version = '7.3.50';
 	//An array of breadcrumbs
 	public $breadcrumbs = array();
 	public $trail = array();

240e971da4d16fa2712c681c4c2e32d352952261
John Havlik <john.havlik@mtekk.us>
2024-12-30 18:35:40 -0600

    7.4 is a go-go

--- class.bcn_admin.php
+++ class.bcn_admin.php
@@ -44,7 +44,7 @@ use mtekk\adminKit\{adminKit, form, message, setting};
  */
 class bcn_admin extends adminKit
 {
-	const version = '7.3.80';
+	const version = '7.4.0';
 	protected $full_name = 'Breadcrumb NavXT Settings';
 	protected $short_name = 'Breadcrumb NavXT';
 	protected $access_level = 'bcn_manage_options';

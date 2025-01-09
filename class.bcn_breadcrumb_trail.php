053d613215f839ce813c96eb44477af7cc00110d
John Havlik <john.havlik@mtekk.us>
2025-01-08 21:12:31 -0600

    Update copyright for 2025

--- class.bcn_breadcrumb_trail.php
+++ class.bcn_breadcrumb_trail.php
@@ -1,6 +1,6 @@
 <?php
 /*
-	Copyright 2015-2024  John Havlik  (email : john.havlik@mtekk.us)
+	Copyright 2015-2025  John Havlik  (email : john.havlik@mtekk.us)
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
